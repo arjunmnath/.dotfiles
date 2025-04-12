f80d064bdba1b0a9d79590201685449aa1015690
Arjun Manjunath <dev.arjunmnath@gmail.com>
2025-04-12 16:09:24 +0530

    added new nvim plugin (smear-cursor)

---
+++ nvim/lua/arjunmnath/plugins/smear-cursor.lua
@@ -0,0 +1,13 @@
+return {
+	"sphamba/smear-cursor.nvim",
+	opts = {
+		smear_between_buffers = true,
+		smear_between_neighbor_lines = true,
+		scroll_buffer_space = true,
+		legacy_computing_symbols_support = false,
+		smear_insert_mode = true,
+		stiffness = 0.8,
+		trailing_stiffness = 0.5,
+		distance_stop_animating = 0.5,
+	},
+}
